123981de63f1b0ae3f500be4ac5b728027d5c5b7
RocioEnriquez22 <rociocenriquez@gmail.com>
2026-09-01 23:29:13 -0300

    empece crud de listar materias

---
+++ controllers/MateriaController.php
@@ -0,0 +1,12 @@
+<?php
+require_once __DIR__ . "/../config/database.php";
+require_once __DIR__ . "/../models/Materia.php";
+//require_once __DIR__ . "/../models/";
+
+class Materia{
+    private $materiaModel;
+    
+
+
+
+}
---
+++ models/Materia.php
@@ -0,0 +1,53 @@
+<?php
+class Materia{
+    private $db;
+
+    public function __construct ($conexion){
+        $this->db =$conexion;
+    }
+
+    //mostramos las materias que estan solo en activas
+    public function getAll(){
+        $sql="SELECT m.idMateria, m.nombre, m.año, m.idEstado, e.nombre AS estado_nombre 
+        from materia m
+        inner join estado e Using (idEstado)
+        where e.activo=1 or e.nombre!= 'Eliminada'
+        order by m.nombre DESC";
+
+        $stmt= $this->db->prepare($sql);
+
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function getById($id){
+        $stmt = $this->db->prepare("SELECT * FROM materia WHERE idMateria = ?");
+        $stmt->execute([$id]);
+        
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function create($nombre, $año, $idEstado){
+        $stmt = $this->db->prepare("INSERT INTO materia (nombre, año, idEstado) VALUES (?, ?, ?)");
+        return $stmt->execute([$nombre, $año, $idEstado]); 
+    }
+
+    public function update($id, $nombre, $año, $idEstado) {
+        $sql="UPDATE materia SET nombre = ?, año = ?, idEstado = ? WHERE idMateria = ?";
+        $stmt = $this->db->prepare($sql);
+        
+        return $stmt->execute([$nombre, $año, $idEstado, $id]);
+    }
+
+    public function deleteLogico($id) {
+        
+        $sql = "UPDATE materia SET idEstado = (
+                    SELECT idEstado FROM estado WHERE nombre = 'Eliminada' OR activo = 0 LIMIT 1
+                ) WHERE idMateria = ?";
+        $stmt = $this->db->prepare($sql);
+        return $stmt->execute([$id]);
+    }
+
+
+
+}
